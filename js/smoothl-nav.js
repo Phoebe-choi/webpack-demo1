1c3de8e04fe7a8f232e9fbc9cdbf84dce079d9d5
Phoebe <675594937@qq.com>
2018-02-01 21:44:53 +0800

    给js添加view和controller,分块整理

--- js/smoothl-nav.js
+++ js/smoothl-nav.js
@@ -1,20 +1,22 @@
 ! function() {
-	let aTags = document.querySelectorAll('nav.menu>ul>li>a')
-
-	function animate(time) {
-		requestAnimationFrame(animate);
-		TWEEN.update(time);
-	}
-	requestAnimationFrame(animate);
-	for (let i = 0; i < aTags.length; i++) {
-		aTags[i].onclick = function(x) {
-			x.preventDefault() //阻止默认动作
-			let a = x.currentTarget //获取用户点击的a标签
-			let href = a.getAttribute('href') //'#siteAbout'
-			let element = document.querySelector(href)
-			// let rect=element.getBoundingClientRect()
+	var view = document.querySelector('nav.menu')
+	var controller = {
+		view: null,
+		aTags: null,
+		init: function(view) {
+			this.view = view
+			this.initAnimation()
+			this.bindEvents()
+		},
+		initAnimation: function() {
+			function animate(time) {
+				requestAnimationFrame(animate);
+				TWEEN.update(time);
+			}
+			requestAnimationFrame(animate);
+		},
+		scrollToElement: function(element) {
 			let top = element.offsetTop //获取与页面顶端的距离
-
 			let currentTop = window.scrollY
 			let targetTop = top - 80
 			let s = targetTop - currentTop //路程
@@ -35,6 +37,20 @@
 					window.scrollTo(0, coords.y) //如何更新界面
 				})
 				.start(); //开始缓动
-		}
+		},
+		bindEvents: function() {
+			let aTags = this.view.querySelectorAll('nav.menu>ul>li>a')
+			for (let i = 0; i < aTags.length; i++) {
+				aTags[i].onclick = (x) => {
+					x.preventDefault() //阻止默认动作
+					let a = x.currentTarget //获取用户点击的a标签
+					let href = a.getAttribute('href') //'#siteAbout'
+					let element = document.querySelector(href)
+					this.scrollToElement(element)
+				}
+			}
+		},
 	}
+	controller.init(view)
+
 }.call()
--- js/stick-topbar.js
+++ js/stick-topbar.js
@@ -1,9 +1,29 @@
-!function() {
-	window.addEventListener('scroll', function(x) {
-		if (window.scrollY > 0) {
-			topNavBar.classList.add('sticky')
-		} else {
-			topNavBar.classList.remove('sticky')
+! function() {
+	var view = document.querySelector('#topNavBar')
+	var controller = {
+		view: null,
+		init: function(view) {//初始化view
+			this.view = view//这个this就是controller
+			this.bindEvents()
+			//this.bindEvents.call(this)
+		},
+		bindEvents: function() { //绑定view事件
+			var view=this.view //这里的this就是第六行的this
+			window.addEventListener('scroll',(x)=> {
+				if (window.scrollY > 0) {
+					this.active()
+				} else {
+					this.deactive()
+				}
+			})
+		},
+		active:function(){ //激活view
+			this.view.classList.add('sticky')
+		},
+		deactive:function(){ //非激活view
+			this.view.classList.remove('sticky')
 		}
-	})
+	}
+	controller.init(view)
+	//controller.init.call(controller,view)
 }.call()
