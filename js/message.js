12db324ca3368ac728bff9e2d3397f445fbc71fa
Phoebe <675594937@qq.com>
2018-02-03 16:33:42 +0800

    添加MVC的Model

--- js/message.js
+++ js/message.js
@@ -1,43 +1,75 @@
-// 初始化
-var APP_ID = '5zC0fMVT28TdVY8l2QfSdDyG-gzGzoHsz';
-var APP_KEY = 'h0yECHy8dC6isqNCMHKHfbA8';
-
-AV.init({
-	appId: APP_ID,
-	appKey: APP_KEY
-});
-
-
-var query = new AV.Query('Message');
-query.find()
-	.then(
-		function(messages) {
-			let array = messages.map((item) => item.attributes)
-			array.forEach((item) => {
-				let li = document.createElement('li')
-				li.innerText = `${item.name}: ${item.content}`
-				let messageList = document.querySelector('#messageList')
-				messageList.appendChild(li)
+! function() {
+	var model = {
+		// 初始化
+		init: function() {
+			var APP_ID = '5zC0fMVT28TdVY8l2QfSdDyG-gzGzoHsz'
+			var APP_KEY = 'h0yECHy8dC6isqNCMHKHfbA8'
+			AV.init({
+				appId: APP_ID,
+				appKey: APP_KEY
+			})
+		},
+		//获取数据
+		fetch: function() {
+			var query = new AV.Query('Message');
+			return query.find() //Promise对象
+		},
+		//创建数据
+		save: function(name, content) {
+			var Message = AV.Object.extend('Message');
+			var message = new Message();
+			return message.save({ //Promise对象
+				'name': name,
+				'content': content
 			})
 		}
-	)
+	}
+	var view = document.querySelector('section.message')
 
-let myForm = document.querySelector('#postMessageForm')
+	var controller = {
+		view: null,
+		model: null,
+		messageList: null,
+		init: function(view,model) {
+			this.view = view
+			this.model = model
 
-myForm.addEventListener('submit', function(e) {
-	e.preventDefault()
-	let content = myForm.querySelector('input[name=content]').value
-	let name = myForm.querySelector('input[name=name]').value
-	var Message = AV.Object.extend('Message');
-	var message = new Message();
-	message.save({
-		'name':name,
-		'content':content
-	}).then(function(object) { //obiect为存入的数据的相关信息
-		let li = document.createElement('li')
-		li.innerText = `${object.attributes.name}:${object.attributes.content}`
-		let messageList = document.querySelector('#messageList')
-		messageList.appendChild(li)
-		myForm.querySelector('input[name=content]').value=''
-	})
-})
+			this.messageList = view.querySelector('#messageList')
+			this.form = view.querySelector('form')
+			this.model.init() //初始化model
+			this.loadMessages() //加载信息
+			this.bindEvents() //绑定事件
+		},
+		loadMessages: function() {
+			this.model.fetch().then(
+				(messages) => {
+					let array = messages.map((item) => item.attributes)
+					array.forEach((item) => {
+						let li = document.createElement('li')
+						li.innerText = `${item.name}: ${item.content}`
+						this.messageList.appendChild(li)
+					})
+				}
+			)
+		},
+		bindEvents: function() {
+			this.form.addEventListener('submit', function(e) {
+				e.preventDefault()
+				this.saveMessage()
+			})
+		},
+		saveMessage: function() {
+			let myForm = this.form
+			let content = myForm.querySelector('input[name=content]').value
+			let name = myForm.querySelector('input[name=name]').value
+			this.model.save(name, content).then(function(object) { //obiect为存入的数据的相关信息
+				let li = document.createElement('li')
+				li.innerText = `${object.attributes.name}:${object.attributes.content}`
+				let messageList = document.querySelector('#messageList')
+				messageList.appendChild(li)
+				myForm.querySelector('input[name=content]').value = ''
+			})
+		}
+	}
+	controller.init(view, model)
+}.call()
